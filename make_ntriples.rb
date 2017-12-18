c937da984dc2a5638957d544fb3fdfa3294652eb
Samuel Yemane Ayele <samzhab@gmail.com>
2017-12-18 20:12:02 +0100

    refactored | rubocopeed

--- make_ntriples.rb
+++ make_ntriples.rb
@@ -11,7 +11,7 @@ def start
     puts '[INPUT] what would you like to do with these? \
                   Z - make page_id triples
                   X - make doi title triples'
-   # choice = gets.chomp
+    # choice = gets.chomp
     choice = 'x'
     case choice
     when 'z'
@@ -48,13 +48,8 @@ def make_doi_nt(tsv_file_name)
   nt_file = File.new(nt_path, 'w') # create nt file same name
   tsv_file = open(Dir.pwd + '/tsv_files/' + tsv_file_name)
   log_file = File.new(Dir.pwd + '/log/' + Time.now.to_s + '.txt', 'w')
-  # file_name = gets + Time.now.to_s
-  # file = File.new(file_name, "w")
-  # puts "content"
-  # file.puts file_name
-  # file.close
   crossref_uri = URI('https://api.crossref.org/v1/works/http://dx.doi.org/')
-  property_url = "http://purl.org/dc/terms/title"
+  property_url = 'http://purl.org/dc/terms/title'
   Net::HTTP.start(crossref_uri.host, crossref_uri.port) do |_http|
     while (line = tsv_file.gets)
       next unless line.split(' ').last.include?("\/")
@@ -67,15 +62,16 @@ def make_doi_nt(tsv_file_name)
       formed_uri = URI(formed_uri)
       response = Net::HTTP.get_response(formed_uri)
       response = check_response(response, id, crossref_url, log_file)
-      puts '[ERROR] resource not found' unless response
-      log_file.puts '[ERROR] resource not found for '  + id.to_s unless response
+      puts '[ERROR] resource not found ' unless response
+      log_file.puts '[ERROR] resource not found for ' + id.to_s unless response
       next unless response
       json_response = JSON.parse(response.body)
       case json_response['status']
       when 'ok'
         titles = json_response['message']['title']
         titles.each do |title|
-          n_triple = "<http://dx.doi.org/" + id + ">" + " <" + property_url + "> " + "\"" + title + "\"."
+          n_triple = '<http://dx.doi.org/' + id + '>' + ' <' + property_url +
+                     '> ' + '"' + title + '".'
           puts n_triple
           nt_file.puts n_triple
           log_file.puts '[INFO] saved triple ---> ' + n_triple
@@ -88,24 +84,23 @@ def make_doi_nt(tsv_file_name)
   log_file.close
 end
 
-def check_response(response, id, c_url, log_file)
+def check_response(response, recheck_id, c_url, log_file)
   case response
   when Net::HTTPSuccess then
     response
   when Net::HTTPNotFound then
     # split id and try again
-    if id.split('/')
-      id = id.split('/')
-      id.pop # remove last part
-      id = id.join('/')
-      formed_uri = Addressable::URI.encode((c_url + id).strip)
-      formed_uri = Addressable::URI.parse(formed_uri)
-      # formed_uri = URI.parse(URI.encode(formed_uri.strip))
-      puts '[INFO] re-trying again as --- ' + formed_uri.to_s
-      log_file.puts '[ERROR] resource not found' unless response
-      response = Net::HTTP.get_response(formed_uri)
-      check_response(response, id, c_url, log_file) unless formed_uri != c_url
-    end
+    # :TODO handle deeper splits
+    recheck_id = recheck_id.split('/')
+    recheck_id.pop # remove last part
+    recheck_id = recheck_id.join('/')
+    formed_uri = Addressable::URI.encode((c_url + recheck_id).strip)
+    formed_uri = Addressable::URI.parse(formed_uri)
+    puts c_url
+    # formed_uri = URI.parse(URI.encode(formed_uri.strip))
+    puts '[INFO] re-trying again as --- ' + formed_uri.to_s
+    response = Net::HTTP.get_response(formed_uri)
+    check_response(response, id, c_url, log_file) unless formed_uri != c_url
   end
 end
 start
